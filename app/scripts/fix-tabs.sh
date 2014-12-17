1b158842418a345a620b977bd19105466b808b5b
Rémi T'JAMPENS <remi.tjampens@gmail.com>
2014-12-17 02:18:31 +0100

    Add a bit of joy.

--- app/scripts/fix-tabs.sh
+++ app/scripts/fix-tabs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo -e "+ Fixing tabs for all PHP files..."
+echo -e "\e[32mFixing tabs for all PHP files...\e[0m"
 for file in $(find . -name "*.php");
 do
-    echo -e "+ $file"
+    echo -e "\e[33m+ $file\e[0m"
     expand -t 4 "$file" > "${file}.tmp" && mv "${file}.tmp" "$file"
 done
-echo -e "+ Done."
+echo -e "\e[32mDone.\e[0m"
